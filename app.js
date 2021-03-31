41ebee03bd96e2d937d687b549c71c201609314f
Vishal Kumar <leader.vishalkumar@gmail.com>
2021-03-31 17:28:35 +0530

    done

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/binary.json"
 
-function translationURL(text) {
-    return serverURL + "?" + "text=" + text;
-}
-
 function errorHandler(error){
     alert("Something wrong. Try again later!!" + error)
 }
 
+function translationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+
 function clickEventHandler() {
     var inputText = textInput.value;
     fetch(translationURL(inputText))
-    .then(response => response.json)
+    .then(response => response.json())
     .then(json => {
-            outputDiv.inputText = json.contents.translated;
-            console.log(json.contents.translated);
+            var outputText = json.contents.translated;
+            outputDiv.innerText = outputText;
+            
         })
     .catch(errorHandler)
 }
